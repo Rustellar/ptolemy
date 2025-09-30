5cf3466731654589073b5caca314d87f27877407
Yasu <ok240108@oks.iput.ac.jp>
2025-09-30 11:27:35 +0900

    Update cc.rs But doesn't work

--- src/cc.rs
+++ src/cc.rs
@@ -39,29 +39,72 @@ pub mod tests {
         // 文書バッファ
         let mut documents = String::new();
 
-        // 1行読み込み
-        let read_bytes = reader.read_line(&mut documents).unwrap();
-        // トリムを行う
-        documents = documents.trim().to_string();
-        
-        if documents.contains(" ") {
-            documents = documents.replace(" ", "。");
+        // デバッグ用
+        let mut _debug_index = 0;
+
+        loop {
+            bar.tick();
+
+            // デバッグ用
+            if _debug_index > 100 {
+                break;
+            }
+
+            _debug_index += 1;
+
+            // 1行読み込み
+            let read_bytes = reader.read_line(&mut documents).unwrap();
+
+            println!("\n----- Debug -----");
+            println!("読み取ったバイト数: {}", read_bytes);
+            println!("読み取った行: {}", documents);
+            println!("行は空? : {}", documents.is_empty());
+            println!("行は空白? : {}", documents.chars().all(char::is_whitespace));
+            println!("-----------------\n");
+
+            // 読み取ったバイトが0?
+            if documents.chars().all(char::is_whitespace) || read_bytes == 0 {
+                // 次の行を読み取り、Noneだった場合はEOFと判断
+                let mut _buf = String::new();
+                if let Err(_) = reader.read_line(&mut _buf) {
+                    println!("EOFに到達しました");
+                    break;
+                }
+                // EOFじゃない場合は、分割を行う
+                else {
+                    println!("-----分割-----");
+                }
+            }
+            else {
+                // トリムを行う
+                documents = documents.trim().to_string();
+            
+                if documents.contains(" ") {
+                    documents = documents.replace(" ", "。");
+                }
+
+                println!("ドキュメント: {}", documents);
+
+                // Stringのクリア
+                documents.clear();
+            }
         }
+    }
 
-        println!("ドキュメント: {:?}", documents);
+    #[test]
+    pub fn print() {
+        let input =
+            "/home/ok240108@OSAKA-NET.nkz.ac.jp/mnt/file_server/ok24/OK240108/data/cc100/ja.txt";
+        let input_file = File::open(input).expect("ファイルが開けません");
+        let reader = BufReader::new(input_file);
 
-        // Stringのクリア
-        documents.clear();
+        for (index, line) in reader.lines().enumerate() {
+            if index > 100 {
+                break;
+            }
 
-        // さらに1行読み込み
-        let read_bytes = reader.read_line(&mut documents).unwrap();
-        // トリムを行う
-        documents = documents.trim().to_string();
-
-        if documents.contains(" ") {
-            documents = documents.replace(" ", "。");
+            let line = line.expect("ファイルが読み込めません");
+            println!("{}: {}", index + 1, line);
         }
-        
-        println!("ドキュメント: {:?}", documents);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, io::{BufRead, BufReader, Read}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Read},
+};
 
 use indicatif::{ProgressBar, ProgressIterator};
 
@@ -7,7 +10,8 @@ use csv::Writer;
 pub mod cc;
 
 fn main() {
-    let input = "/home/ok240108@OSAKA-NET.nkz.ac.jp/mnt/file_server/ok24/OK240108/data/cc100/ja.txt";
+    let input =
+        "/home/ok240108@OSAKA-NET.nkz.ac.jp/mnt/file_server/ok24/OK240108/data/cc100/ja.txt";
     let output = "/home/ok240108@OSAKA-NET.nkz.ac.jp/mnt/file_server/ok24/OK240108/data/cc100/ja";
 
     let input_file = File::open(input).expect("ファイルが開けません");
@@ -24,7 +28,7 @@ fn main() {
 
     // 文字塊のインデックス
     let mut chunk_index = 1;
-        
+
     // ここを改良する必要あり
     for (index, line) in reader.lines().enumerate() {
         let line = line.expect("行の読み込みに失敗しました");
@@ -42,19 +46,25 @@ fn main() {
                 documents.push(buffer.trim().to_string());
 
                 // csvに書き込み
-                let output = File::create(format!("{}_chunk_{}.csv", output, chunk_index)).expect("ファイルの作成に失敗しました");
+                let output = File::create(format!("{}_chunk_{}.csv", output, chunk_index))
+                    .expect("ファイルの作成に失敗しました");
                 let mut writter = csv::Writer::from_writer(&output);
                 // レコード書き込み
-                writter.write_record(&["index", "text"]).expect("ヘッダーの書き込みに失敗しました");
+                writter
+                    .write_record(&["index", "text"])
+                    .expect("ヘッダーの書き込みに失敗しました");
                 // データを書き込む
-                writter.write_record(&[index.to_string()]).expect("書き込みに失敗しました");
-                writter.write_record(&[documents.join("\n")]).expect("書き込みに失敗しました");
+                writter
+                    .write_record(&[index.to_string()])
+                    .expect("書き込みに失敗しました");
+                writter
+                    .write_record(&[documents.join("\n")])
+                    .expect("書き込みに失敗しました");
                 writter.flush().expect("フラッシュに失敗しました");
                 buffer.clear();
                 chunk_index += 1;
             }
-        }
-        else {
+        } else {
             // バッファに行を追加
             buffer.push_str(&line);
             buffer.push('\n');
